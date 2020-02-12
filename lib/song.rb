6574a33b53d855137327ad0f72ea8436842f1471
Samantha Meason <sbmeason@gmail.com>
2020-02-12 21:06:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,28 @@
-class Song 
+class Song
+  attr_accessor :name, :artist, :genre
+
   @@count = 0
-  
-  def self.count 
+  @@genres = []
+  @@artists = []
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
     @@count
-  end 
-  
-def initialize
-  @@song
-  @@artists
-  @@genres
-end
-  
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
 end
