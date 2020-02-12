1b9e6a5b246de6091a2ed21fde6f1b070888bddb
Samantha Meason <sbmeason@gmail.com>
2020-02-12 21:20:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,12 +40,13 @@ def self.genre_count
 def self.artist_count
   artist_count = {}
    @@artists.each do |artist|
-     if artist_count[genre]
-       genre_count[genre] += 1 
+     if artist_count[artist]
+       artist_count[artist] += 1 
      else
-       genre_count = 1
+       artist_count = 1
      end
-   genre_count
+   artist_count
+ end
  end
  
   
